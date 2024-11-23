358b0597799fa8e92dc0cd4c134477f109773af7
br1xd <br1robinsonnn@gmail.com>
2024-11-23 01:06:03 -0300

    Se agregó un botón para que el usuario pueda cambiar de usuario en caso de intentar entrar con una cuenta inhabilitada. Version 0.0.55

--- app/src/main/java/com/example/testmapboxkotlin/view/AuthActivity.java
+++ app/src/main/java/com/example/testmapboxkotlin/view/AuthActivity.java
@@ -3,8 +3,10 @@ package com.example.testmapboxkotlin.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,17 +19,21 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 import java.util.Objects;
 
 public class AuthActivity extends AppCompatActivity {
 
     private Button submit_btn;
+    private Button changeuser_btn;
 
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
@@ -97,6 +103,34 @@ public class AuthActivity extends AppCompatActivity {
                         updateUI(user);
                     } else {
                         Log.w("GoogleSignIn", "signInWithCredential:failure", task.getException());
+                        Toast.makeText(getApplicationContext(),"Esta cuenta esta inhabilitada",Toast.LENGTH_SHORT).show();
+                        changeuser_btn = findViewById(R.id.btn_changeuser);
+                        changeuser_btn.setVisibility(View.VISIBLE);
+                        String web_client_id = getString(R.string.default_web_client_id);
+                        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                                .requestIdToken(web_client_id) // Asegúrate de tener este ID en strings.xml
+                                .requestEmail()
+                                .build();
+                        GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+                        changeuser_btn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                // Cerrar sesión en Google
+                                googleSignInClient.signOut()
+                                        .addOnCompleteListener(AuthActivity.this, new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                // Cerrar sesión en Firebase
+                                                mAuth.signOut();
+                                                Intent signInIntent = googleSignInClient.getSignInIntent();
+                                                startActivityForResult(signInIntent, RC_SIGN_IN);
+                                                // Ahora el usuario está desconectado, puedes redirigir a otra actividad
+
+                                            }
+                                        });
+                            }
+                        });
                         updateUI(null);
                     }
                 });
